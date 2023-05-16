2da8fad8debab799db70d35a0d9406929c743864
Goose <56400167+TheLostNarwhal@users.noreply.github.com>
2023-05-16 00:42:32 -0500

    finally started working on it again

--- world-map/src/app/app-routing.module.ts
+++ world-map/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
-  {path: 'map', component: MapComponent}
+  {path: '', component: MapComponent}
 ];
 
 @NgModule({
