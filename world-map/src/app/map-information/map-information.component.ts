d1ff99ee55cd02858024115a1a5d8c1fefe1b167
Goose <56400167+TheLostNarwhal@users.noreply.github.com>
2023-05-17 23:36:40 -0500

    update

--- world-map/src/app/map-information/map-information.component.ts
+++ world-map/src/app/map-information/map-information.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-map-information',
   templateUrl: './map-information.component.html',
   styleUrls: ['./map-information.component.css']
 })
-export class MapInformationComponent {
 
+export class MapInformationComponent implements OnInit {
+  bankerName = "Elizabeth Franklin";
+  bankerAddress = '123 Main Street';
+  bankerCity = 'Hoboken';
+  bankerState = 'NJ';
+  bankerZip = '07030';
+  bankerPhone = '(201) 748-6000';
+  bankerEmail = 'efranklin@email.com'; 
+  summary = 'Personable and organized Banker with 6+ years of experience matching customers with suitable account solutions and consistently surpassing quotas for new checking, savings, and credit card accounts.';
+  school = 'University of Chicago';
+  schoolDates = 'September 2009 - December 2013';
+  schoolDegree = 'Bachelor of Arts, Finance';
+  employerName = 'Tradelot';
+  jobTitle = 'Banker';
+  employmentDates = 'January 2014 - Present';
+
+ 
+
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }
