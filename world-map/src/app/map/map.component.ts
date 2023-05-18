663bd8e1013a8be70a0903fb86d833a5a878286a
Goose <56400167+TheLostNarwhal@users.noreply.github.com>
2023-05-18 00:04:48 -0500

    update

--- world-map/src/app/map/map.component.ts
+++ world-map/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener,ElementRef, Input} from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class MapComponent {
   // Your component logic goes here
-  bankerName = "Henry";
-  BankerAddress = "1"
+ 
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    const svgElement = this.elementRef.nativeElement.querySelector('svg');
+    const pathElements = svgElement.querySelectorAll('path');
+    pathElements.forEach((path: SVGPathElement) => {
+      /*path.addEventListener('mouseover', this.handleMouseOver.bind(this)); */
+      path.addEventListener('mouseout', this.handleMouseOut.bind(this));
+      path.addEventListener('click', this.handleClick.bind(this));
+    });
+  }
+
+  /*handleMouseOver(event: MouseEvent) {
+    const path = event.target as SVGPathElement;
+    const countryId = path.id;
+    // Emit the countryId to the parent component or perform other actions
+  } */
+
+  handleMouseOut(event: MouseEvent) {
+    // Handle mouse out event if needed
+    
+  }
+
+  handleClick(event: MouseEvent) {
+    const path = event.target as SVGPathElement;
+    const countryId = path.id;
+    alert(countryId);
+    // Emit the countryId to the parent component or perform other actions
+  }
 }
+
+
