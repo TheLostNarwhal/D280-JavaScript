e42f602cc1afbccaa6c846ab24ce9159fd409236
Goose <56400167+TheLostNarwhal@users.noreply.github.com>
2023-05-19 02:09:57 -0500

    final update

--- world-map/src/app/map/map.component.ts
+++ world-map/src/app/map/map.component.ts
@@ -8,7 +8,7 @@ import { CountryApiService } from '../api.service';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-  // Your component logic goes here
+
  
   constructor(
     private elementRef: ElementRef,
@@ -20,51 +20,48 @@ export class MapComponent {
     const svgElement = this.elementRef.nativeElement.querySelector('svg');
     const pathElements = svgElement.querySelectorAll('path');
     pathElements.forEach((path: SVGPathElement) => {
-      /*path.addEventListener('mouseover', this.handleMouseOver.bind(this)); */
-      path.addEventListener('mouseout', this.handleMouseOut.bind(this));
+      
+      
       path.addEventListener('click', this.handleClick.bind(this));
     });
   }
 
-  /*handleMouseOver(event: MouseEvent) {
-    const path = event.target as SVGPathElement;
-    const countryId = path.id;
-    // Emit the countryId to the parent component or perform other actions
-  } */
+ 
 
-  handleMouseOut(event: MouseEvent) {
-    // Handle mouse out event if needed
-    
-  }
+
   @Output() svgIdSelected = new EventEmitter<string>();
   @Output() incomeSelected = new EventEmitter<string>();
+  @Output() capitalSelected = new EventEmitter<string>();
+  @Output() longitudeSelected = new EventEmitter<string>();
+  @Output() latitudeSelected = new EventEmitter<string>();
+  @Output() regionSelected = new EventEmitter<string>();
   @ViewChild(MapInformationComponent)
 private mapInformationComponent!: MapInformationComponent;
 
 handleClick(event: MouseEvent) {
   const path = event.target as SVGPathElement;
   const countryId = path.id;
-  //this.svgIdSelected.emit(countryId);
+
 
   this.countryApiService.getCountryInformation(countryId).subscribe((data: any) => {
-    // Extract the necessary information from the API response
     const countryName = data[1][0].name;
-    const population = data[1][0].population;
     const income = data[1][0].incomeLevel.value;
     const capitalCity = data[1][0].capitalCity;
     const longitude = data[1][0].longitude;
     const latitude = data[1][0].latitude;
     const region = data[1][0].region.value;
-    // Extract three other items as needed
-    console.log(countryName);
-    console.log(population)
+
+
+
     this.svgIdSelected.emit(countryName);
     this.incomeSelected.emit(income);
+   
+    this.capitalSelected.emit(capitalCity);
+    this.longitudeSelected.emit(longitude);
+    this.latitudeSelected.emit(latitude);
+    this.regionSelected.emit(region);
 
-    // Pass the retrieved information to the MapInformationComponent if it's available
-    if (this.mapInformationComponent) {
-      this.mapInformationComponent.setCountryInformation(countryName, population, income);
-    }
+    
   });
 }
 
